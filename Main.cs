a38f3a24c830f36fb10ab974780debaddda26bdc
joerkig <44418429+joerkig@users.noreply.github.com>
2021-04-14 01:57:09 +0200

    Cleaned up a bit

--- Main.cs
+++ Main.cs
@@ -8,96 +8,48 @@ namespace SimpleRewards
 {
     public class MyMod : MelonMod
     {
-        public override void OnLateUpdate()
+        static void Blank(string position)
+        {
+            if (File.ReadAllText(MelonUtils.UserDataDirectory + "\\DisplayName" + position + ".txt") != null)
+            {
+                File.WriteAllText(MelonUtils.UserDataDirectory + "\\DisplayName" + position + ".txt", null);
+                File.WriteAllText(MelonUtils.UserDataDirectory + "\\DisplayDescription" + position + ".txt", null);
+            }
+        }
+        static void WriteInfo(string position)
         {
             string pattern = "(<script(\\s|\\S)*?<\\/script>)|(<style(\\s|\\S)*?<\\/style>)|(<!--(\\s|\\S)*?-->)|(<\\/?(\\s|\\S)*?>)";
-            GameObject left = GameObject.Find("RewardSpawner/SpawnPoints/Left");
-            if (left != null)
+            GameObject gameObject = GameObject.Find("RewardSpawner/SpawnPoints/" + position);
+            if (gameObject != null)
             {
-                RewardInteractable rewardInteractable = left.GetComponentInChildren<RewardInteractable>();
+                RewardInteractable rewardInteractable = gameObject.GetComponentInChildren<RewardInteractable>();
                 if (rewardInteractable != null)
                 {
-                    if (rewardInteractable.DisplayName.ToString() != File.ReadAllText(MelonLoaderBase.UserDataPath + "\\DisplayNameLeft.txt"))
+                    if (rewardInteractable.DisplayName.ToString() != File.ReadAllText(MelonUtils.UserDataDirectory + "\\DisplayName" + position + ".txt"))
                     {
-                        File.WriteAllText(MelonLoaderBase.UserDataPath + "\\DisplayNameLeft.txt", rewardInteractable.DisplayName.ToString());
-                        File.WriteAllText(MelonLoaderBase.UserDataPath + "\\DisplayDescriptionLeft.txt", Regex.Replace(rewardInteractable.DisplayDescription.ToString(), pattern, ""));
-                        //MelonLogger.Log(rewardInteractable.DisplayName.ToString());
-                        //MelonLogger.Log(rewardInteractable.DisplayDescription.ToString());
-                    }
-                }
-                else
-                {
-                    if (File.ReadAllText(MelonLoaderBase.UserDataPath + "\\DisplayNameLeft.txt") != null)
-                    {
-                        File.WriteAllText(MelonLoaderBase.UserDataPath + "\\DisplayNameLeft.txt", null);
-                        File.WriteAllText(MelonLoaderBase.UserDataPath + "\\DisplayDescriptionLeft.txt", null);
-                    }
-                }
-            }
-            GameObject center = GameObject.Find("RewardSpawner/SpawnPoints/Center");
-            if (center != null)
-            {
-                RewardInteractable rewardInteractable = center.GetComponentInChildren<RewardInteractable>();
-                if (rewardInteractable != null)
-                {
-                    if (rewardInteractable.DisplayName.ToString() != File.ReadAllText(MelonLoaderBase.UserDataPath + "\\DisplayNameCenter.txt"))
-                    {
-                        File.WriteAllText(MelonLoaderBase.UserDataPath + "\\DisplayNameCenter.txt", rewardInteractable.DisplayName.ToString());
-                        File.WriteAllText(MelonLoaderBase.UserDataPath + "\\DisplayDescriptionCenter.txt", Regex.Replace(rewardInteractable.DisplayDescription.ToString(), pattern, ""));
-                        //MelonLogger.Log(rewardInteractable.DisplayName.ToString());
-                        //MelonLogger.Log(rewardInteractable.DisplayDescription.ToString());
-                    }
-                }
-                else
-                {
-                    if (File.ReadAllText(MelonLoaderBase.UserDataPath + "\\DisplayNameCenter.txt") != null)
-                    {
-                        File.WriteAllText(MelonLoaderBase.UserDataPath + "\\DisplayNameCenter.txt", null);
-                        File.WriteAllText(MelonLoaderBase.UserDataPath + "\\DisplayDescriptionCenter.txt", null);
-                    }
-                }
-            }
-            GameObject right = GameObject.Find("RewardSpawner/SpawnPoints/Right");
-            if (right != null)//prevents the code from crashing the game while referencing a null object
-            {
-                RewardInteractable rewardInteractable = right.GetComponentInChildren<RewardInteractable>();
-                if (rewardInteractable != null)
-                {
-                    if (rewardInteractable.DisplayName.ToString() != File.ReadAllText(MelonLoaderBase.UserDataPath + "\\DisplayNameRight.txt"))
-                    {
-                        File.WriteAllText(MelonLoaderBase.UserDataPath + "\\DisplayNameRight.txt", rewardInteractable.DisplayName.ToString());
-                        File.WriteAllText(MelonLoaderBase.UserDataPath + "\\DisplayDescriptionRight.txt", Regex.Replace(rewardInteractable.DisplayDescription.ToString(), pattern, ""));
-                        //MelonLogger.Log(rewardInteractable.DisplayName.ToString());
-                        //MelonLogger.Log(rewardInteractable.DisplayDescription.ToString());
-                    }
-                }
-                else
-                {
-                    if (File.ReadAllText(MelonLoaderBase.UserDataPath + "\\DisplayNameRight.txt") != null)
-                    {
-                        File.WriteAllText(MelonLoaderBase.UserDataPath + "\\DisplayNameRight.txt", null);
-                        File.WriteAllText(MelonLoaderBase.UserDataPath + "\\DisplayDescriptionRight.txt", null);
+                        File.WriteAllText(MelonUtils.UserDataDirectory + "\\DisplayName" + position + ".txt", rewardInteractable.DisplayName.ToString());
+                        File.WriteAllText(MelonUtils.UserDataDirectory + "\\DisplayDescription" + position + ".txt", Regex.Replace(rewardInteractable.DisplayDescription.ToString(), pattern, ""));
                     }
                 }
             }
         }
+        public override void OnLateUpdate()
+        {
+            WriteInfo("Left");
+            WriteInfo("Center");
+            WriteInfo("Right");
+        }
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+        {
+            Blank("Left");
+            Blank("Center");
+            Blank("Right");
+        }
         public override void OnApplicationQuit()
         {
-            if (File.ReadAllText(MelonLoaderBase.UserDataPath + "\\DisplayNameLeft.txt") != null)
-            {
-                File.WriteAllText(MelonLoaderBase.UserDataPath + "\\DisplayNameLeft.txt", null);
-                File.WriteAllText(MelonLoaderBase.UserDataPath + "\\DisplayDescriptionLeft.txt", null);
-            }
-            if (File.ReadAllText(MelonLoaderBase.UserDataPath + "\\DisplayNameCenter.txt") != null)
-            {
-                File.WriteAllText(MelonLoaderBase.UserDataPath + "\\DisplayNameCenter.txt", null);
-                File.WriteAllText(MelonLoaderBase.UserDataPath + "\\DisplayDescriptionCenter.txt", null);
-            }
-            if (File.ReadAllText(MelonLoaderBase.UserDataPath + "\\DisplayNameRight.txt") != null)
-            {
-                File.WriteAllText(MelonLoaderBase.UserDataPath + "\\DisplayNameRight.txt", null);
-                File.WriteAllText(MelonLoaderBase.UserDataPath + "\\DisplayDescriptionRight.txt", null);
-            }
+            Blank("Left");
+            Blank("Center");
+            Blank("Right");
         }
     }
 }
